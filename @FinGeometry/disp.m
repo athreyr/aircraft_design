6f8a1ccc4869551325814808bd9e37b036b930fe
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2021-04-06 23:13:50 +0530

    [FinGeometry] Modify disp for better print formatting

--- @FinGeometry/disp.m
+++ @FinGeometry/disp.m
@@ -1,13 +1,14 @@
 function disp(ThisFinGeometry)
-% Overloaded function to control command line display behaviour
+% disp overloads the builtin disp function for ThisFinGeometry class
 
-fprintf('Reference wing with the following geometry:\n')
-fprintf('Reference area     [L^2]   =   %g\n',ThisFinGeometry.referenceArea)
-fprintf('Aspect ratio       [-]     =   %g\n',ThisFinGeometry.aspectRatio)
-fprintf('Taper ratio        [-]     =   %g\n',ThisFinGeometry.taperRatio)
-fprintf('Full span          [L]     =   %g\n',ThisFinGeometry.fullspan)
-fprintf('Root chord         [L]     =   %g\n',ThisFinGeometry.rootChord)
-fprintf('Sweep angle        [-]     =   %g deg\n',ThisFinGeometry.sweepAngle_deg)
-fprintf('Location of sweep  [-]     =   %g%%\n',ThisFinGeometry.sweepLocation * 100)
+fprintf('FinGeometry object having the following properties:\n')
+fprintf('Reference area             [L^2]   =   %g\n', ThisFinGeometry.referenceArea)
+fprintf('Aspect ratio               [-]     =   %g\n', ThisFinGeometry.aspectRatio)
+fprintf('Taper ratio                [-]     =   %g\n', ThisFinGeometry.taperRatio)
+fprintf('Full span                  [L]     =   %g\n', ThisFinGeometry.fullspan)
+fprintf('Root chord                 [L]     =   %g\n', ThisFinGeometry.rootChord)
+fprintf('Sweep angle                [-]     =   %g deg\n', ThisFinGeometry.sweepAngle_deg)
+fprintf('Location of constant sweep [-]     =   %g%% of chord\n', ...
+                                                        ThisFinGeometry.sweepLocation * 100)
 
 end
