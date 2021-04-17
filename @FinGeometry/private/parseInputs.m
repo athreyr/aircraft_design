6262929d347fa3610767f7ba5fe8504c47a04ee5
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2021-04-17 17:12:18 +0530

    [FinGeometry] Return property names also after input parsing
    If only values are returned from parseInputs, they may not be assigned to
    the correct property in the constructor, because the property names are
    read from the defaults file, and their ordering cannot be guaranteed (it
    could, but that's inconvenient for the user to never change the order).
    
    So, the names of the properties corresponding to each parsed value are
    also returned from parseInputs.

--- @FinGeometry/private/parseInputs.m
+++ @FinGeometry/private/parseInputs.m
@@ -1,18 +1,28 @@
-function inputValues = parseInputs(S, AR, defaultValues, varargin)
+function [inputPropertiesList, valuesList] = ...
+                        parseInputs(S, AR, defaultValues, userInputValues)
 % parseInputs parses and validates user inputs that contain optional inputs
 % 
 %   Helper function for class constructor.
 % 
 %   Meant for internal use only.
 
-[TR, lambda, lambdaRelPos] = setOptionalInputs(defaultValues, varargin{:});
+[TR, lambda_deg, lambdaRelPos] = setOptionalInputs(defaultValues, userInputValues);
+valuesList = {S; AR; TR; lambda_deg; lambdaRelPos};
+inputPropertiesList = {'referenceArea'; ...
+                       'aspectRatio'; ...
+                       'taperRatio'; ...
+                       'sweepAngle_deg'; ...
+                       'sweepLocation'};
 
 % validate user inputs
-try
-    inputValues = [S, AR, TR, lambda, lambdaRelPos]; % may throw horzcat error 
-    validateattributes(inputValues, {'numeric'}, {'real', 'finite', 'vector'})
-catch ME
-    throwAsCaller(ME)
+for argidx = 1:numel(valuesList)
+    try
+        validateattributes(valuesList{argidx}, {'numeric'}, {'real', 'finite', 'scalar'})
+    catch ME
+        foostr = sprintf('Parse error at user input number %i:\n', argidx);
+        ME = MException(ME.identifier, [foostr, ME.message]);
+        ME.throwAsCaller
+    end
 end
 
 % issue warnings about potentially infeasible physical values
@@ -29,7 +39,7 @@ if TR < 0 || TR > 1
             TR)
 end
 
-if lambda < -180 || lambda > 180
+if lambda_deg < -180 || lambda_deg > 180
     warning('FinGeometry:sweepAngle_deg:outOfBounds',...
             'Sweep angle (%3.1f deg) is not between -180 deg and 180 deg.',...
             sweepAngle_deg)
