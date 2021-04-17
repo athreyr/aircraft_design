ef77683dd105f88e87ea62b5fa3f7302a717847b
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2021-04-17 17:18:39 +0530

    [FinGeometry] Make class definition consistent with others

--- @FinGeometry/FinGeometry.m
+++ @FinGeometry/FinGeometry.m
@@ -13,36 +13,42 @@ classdef FinGeometry
     properties (SetAccess = private)        
         referenceArea % S [L^2] (user input)
         aspectRatio % AR = b^2 / S [-] (user input)
-        taperRatio % TR = cRoot / cTip [-] (user input)        
+        taperRatio % TR = cRoot / cTip [-] (user input)
+        
         fullspan % b = sqrt(S * AR) [L] (computed)
         rootChord % cRoot = 2 * S / (b * (1 + TR)) [L] (computed)
+        meanAerodynamicChord % cBar = 2/3 *cRoot *(1+TR+TR^2)/(1+TR) [L] (computed)
+        macLocation % yBar = b/6 * (1+2*TR) / (1+TR) [L] (computed)
     end
     
-    properties % public because these are only used for plot()
+    properties % SetAccess = public because these are only used for plot()
         sweepAngle_deg % lambda [-] (user input)
-        sweepLocation % lambdaRelPos is the x/c where lambda is constant [-] (user input)
+        sweepLocation % xBarLambda is the x/c where lambda is constant [-] (user input)
     end
     
     methods % public
         
         % constructor should be in the same m-file as that of class
         function ThisFinGeometry = FinGeometry(S, AR, varargin)
-            % ThisFinGeometry = FinGeometry(S, AR, [TR = 1], [lambda = 0], [lambdaRelPos = 0])
+        % ThisFinGeometry = FinGeometry(S, AR, [TR = 1], [lambda = 0], [xBarLambda = 0])
             
             % get names of input properties and their default values
-            [inputPropertiesList, inputValues] = defaultInputs('FinGeometry');
+            Default = defaultInputs('FinGeometry');
             
+            argin = {};
             if nargin > 0 % constructor should work with no input
                 narginchk(2,5) % according to the multiple function signatures
-                optionalInputsList = {'taperRatio'; 'sweepAngle_deg'; 'sweepLocation'};
-                [~, optionalInputsIdx] = ismember(optionalInputsList, inputPropertiesList);
-                defaultValues = inputValues(optionalInputsIdx);
-                inputValues = parseInputs(S, AR, defaultValues, varargin{:}).';
+%                 optionalInputsList = {'taperRatio'; 'sweepAngle_deg'; 'sweepLocation'};
+%                 [~, optionalInputsIdx] = ismember(optionalInputsList, propertiesList);
+%                 defaultValues = valuesList(optionalInputsIdx).';
+                argin = [S, AR, varargin];
             end
+            
+            [inputPropertiesList, valuesList] = parseInputs(Default, argin);
             
             % assign input properties to object
             for idx = 1:numel(inputPropertiesList)
-                ThisFinGeometry.(inputPropertiesList{idx}) = inputValues(idx);
+                ThisFinGeometry.(inputPropertiesList{idx}) = valuesList{idx};
             end
             
             % compute the remaining properties from inputs
@@ -51,6 +57,7 @@ classdef FinGeometry
         end % constructor
         
         % include the function signatures of the other methods
+        sweepAngle = sweep(ThisFinGeometry, xBar, varargin);
         chordlength = chord(ThisFinGeometry, y);
         disp(ThisFinGeometry)
         varargout = plot(ThisFinGeometry, varargin);
