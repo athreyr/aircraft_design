9d120f0d8330e228f172114fbb8bf45a8b7054b4
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2021-04-17 17:23:04 +0530

    [FoilGeometry] Make class definition consistent with others

--- @FoilGeometry/FoilGeometry.m
+++ @FoilGeometry/FoilGeometry.m
@@ -2,44 +2,73 @@ classdef FoilGeometry
     %FOILGEOMETRY Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties (SetAccess = protected)
+    properties (SetAccess = private)
         abcissas
         ordinates
         perimeter
         area
-        centroidPerimeter
+        centroidPerimeter % "perimeter centroid" is a real thing
         centroidArea
         thicknessChord
 %         thicknessCamber
         camber
+        coordinatesFormat % computed (only 'selig' or 'lednicer' for now)
+    end
+    
+    properties % public
+        name % defaults to name of datasrc, just change afterwards
     end
     
     methods
         function ThisFoilGeometry = FoilGeometry(varargin)
-            %FOILGEOMETRY Construct an instance of this class
-            %   Detailed explanation goes here
+        % ThisFoilGeometry = FoilGeometry(datasrc, [nHeaderRows = 1], [nLeadCols = 0])
+        % ThisFoilGeometry = FoilGeometry(abcissas, ordinates, [name = 'untitled'])
+        
+            % get names of input properties and their default values
+            Default = defaultInputs('FoilGeometry');
             
-            UserInputs = FoilGeometry.unpackInputs(varargin);            
-            Props = FoilGeometry.computeProperties(UserInputs);
+            if nargin > 0 % constructor should work with no input
+                narginchk(1,3) % according to the multiple function signatures
+%                 optionalInputsList = {'abcissas', 'ordinates', 'name'};
+%                 [~, optionalInputsIdx] = ismember(optionalInputsList, propertiesList);
+%                 defaultValues = valuesList(optionalInputsIdx).';
+%                 defaultUserInputValues = {1, 0};
+%                 defaultValues = [defaultUserInputValues, defaultPropertyValues];
+%                 [propertiesList, valuesList] = parseInputs(defaultValues, varargin);
+            end
             
-            ThisFoilGeometry.abcissas = Props.abcissas;
-            ThisFoilGeometry.ordinates = Props.ordinates;
-            ThisFoilGeometry.perimeter = Props.perimeter;
-            ThisFoilGeometry.area = Props.area;
-            ThisFoilGeometry.centroidPerimeter = Props.centroidPerimeter;
-            ThisFoilGeometry.centroidArea = Props.centroidArea;
-            ThisFoilGeometry.thicknessChord = Props.thicknessChord;
-%             ThisFoilGeometry.thicknessCamber = Props.thicknessCamber;
-            ThisFoilGeometry.camber = Props.camber;
+            [inputPropertiesList, valuesList] = parseInputs(Default, varargin);
+            
+            % assign input properties to object
+            for iProp = 1:numel(inputPropertiesList)
+                ThisFoilGeometry.(inputPropertiesList{iProp}) = valuesList{iProp};
+            end
+            
+            % compute the remaining properties from inputs
+            ThisFoilGeometry = ThisFoilGeometry.setComputedProperties;
             
         end
+        
+        [tmax, xT] = maxThickness(ThisFoilGeometry);
+        [cMax, xC] = maxCamber(ThisFoilGeometry);
+        disp(ThisFoilGeometry)
+        varargout = plot(ThisFoilGeometry, varargin);
+        [xnew, ynew] = switchCoordinatesFormat(ThisFoilGeometry, toFormat);
     end
     
-    methods (Static)
-        UserInputs = unpackInputs(varargin);
-        Props = computeProperties(UserInputs);
-        [newAbcissas,newOrdinates] = makeValid(abcissas,ordinates);
-        varargout = convertDataFormat(dataPoints,toFormatName);
+    methods (Access = private, Hidden)
+        ThisFoilGeometry = setComputedProperties(ThisFoilGeometry);
+        ThisFoilGeometry = setCoordinatesFormat(ThisFoilGeometry);
     end
+    %{
+%     methods (Static)
+%         coordFormat = determineCoordinatesFormat(coords);
+%         [xnew, ynew] = switchCoordinatesFormat(x, y, fromFormat, toFormat);
+%         UserInputs = unpackInputs(varargin);
+%         Props = computeProperties(UserInputs);
+%         [newAbcissas,newOrdinates] = makeValid(abcissas,ordinates);
+%         varargout = convertDataFormat(dataPoints,toFormatName);
+%     end
+    %}
 end
 
--- @FoilGeometry/setComputedProperties.m
+++ @FoilGeometry/setComputedProperties.m
@@ -1,55 +1,48 @@
-function Props = computeProperties(UserInputs)
+function ThisFoilGeometry = setComputedProperties(ThisFoilGeometry)
 %COMPUTEPROPERTIES Summary of this function goes here
 %   Detailed explanation goes here
 
-% Inititalise output structure so we know what fields it would have
-Props = struct('abcissas', 0, ...
-               'ordinates', 0, ...
-               'perimeter', 0, ...
-               'area', 0, ...
-               'centroidPerimeter', [0,0], ...
-               'centroidArea', [0,0], ...
-               'thicknessChord', 0, ...
-               ...'thicknessCamber', 0, ...
-               'camber', 0);
-fid = fopen('defaultObjectProperties.csv');
-scannedCells = textscan(fid,'%s,%d');
-% Props = cell2struct(scannedCells(:,2),scannedCells(:,1),1);
-fields = scannedCells{1};
-vars = scannedCells{2};
-for thisField = 1:numel(fields)
-    Props.(fields(thisField)) = vars(thisField);
-end
-%{
-[Props.abcissas, Props.ordinates] = FoilGeometry.makeValid(UserInputs.x, UserInputs.y);
+% abcissas = ThisFoilGeometry.abcissas;
+% ordinates = ThisFoilGeometry.ordinates;
 
-ds = [0; sqrt(diff(Props.abcissas).^2 + diff(Props.ordinates).^2)];
-Props.perimeter = sum(ds);
-Props.centroidPerimeter = 1/Props.perimeter * ...
-                        [sum(Props.ordinates.*ds), sum(Props.abcissas.*ds)];
+% use fixAirfoil here
+% check for compatible array sizes in x and y (make both column vectors)
+% check for valid filenames, vars
+% write default file for inputs
+% spline interpolate in x and y to make the points good
 
-Props.area = trapz(Props.abcissas,Props.ordinates);
+% coordFormat = FoilGeometry.determineCoordinatesFormat([abcissas, ordinates]);
+ThisFoilGeometry = ThisFoilGeometry.setCoordinatesFormat;
 
-yPos = Props.ordinates(Props.ordinates > 0);
-yNeg = Props.ordinates(Props.ordinates < 0);
-                    
-dA = [0; diff(Props.abcissas)/2 .* [diff()]];
-%}
-               
-[x,y] = FoilGeometry.convertDataFormat([UserInputs.x, UserInputs.y], 'selig');
-ds = [0; sqrt(diff(x).^2 + diff(y).^2)];
-Props.perimeter = sum(ds);
-Props.centroidPerimeter = [sum(x.*ds), sum(y.*ds)] / Props.perimeter;
+% [x, y] = FoilGeometry.switchCoordinatesFormat(abcissas, ordinates, ...
+%                                               coordFormat, 'selig');
+[x, y] = ThisFoilGeometry.switchCoordinatesFormat('selig');
+dS = [0; sqrt(diff(x).^2 + diff(y).^2)];
+S = sum(dS);
+Cperimeter = [sum(x.*dS), sum(y.*dS)] / S;
 
-[xcommon, yupper, ylower] = ...
-    FoilGeometry.convertDataFormat([UserInputs.x, UserInputs.y], 'lednicer');
-Props.thicknessChord = yupper - ylower;
-Props.camber = Props.thicknessChord / 2;
+% [xcoords, ycoords] = FoilGeometry.switchCoordinatesFormat(abcissas, ...
+%                                         ordinates, coordFormat, 'lednicer');
+[xcoords, ycoords] = ThisFoilGeometry.switchCoordinatesFormat('lednicer');
+nPts = numel(xcoords) / 2;
+xcommon = (xcoords(1:nPts) + xcoords(nPts+1:end)) / 2;
+yupper = ycoords(1:nPts);
+ylower = ycoords(nPts+1:end);
+tChord = yupper - ylower;
+camberPts = (yupper + ylower) / 2;
 
-dA = [0; diff(xcommon)/2 .* (Props.thicknessChord(1:end-1) + Props.thicknessChord(2:end))];
-Props.area = sum(dA);
-Props.centroidArea = [sum(xcommon .* dA), sum(Props.camber .* dA)] / Props.area;
+dA = [0; diff(xcommon)/2 .* (tChord(1:end-1) + tChord(2:end))];
+A = sum(dA);
+xPts = [0; (xcommon(1:end-1) + xcommon(2:end))/2];
+Carea = [sum(xPts .* dA), sum(camberPts .* dA)] / A;
 
+% ThisFoilGeometry.coordinatesFormat = coordFormat;
+ThisFoilGeometry.perimeter = S;
+ThisFoilGeometry.area = A;
+ThisFoilGeometry.centroidPerimeter = Cperimeter;
+ThisFoilGeometry.centroidArea = Carea;
+ThisFoilGeometry.thicknessChord = [tChord, xcommon];
+ThisFoilGeometry.camber = [camberPts, xcommon];
 
 end
 
