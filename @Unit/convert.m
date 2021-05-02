0ace365c8d40ce3ddca67a5fb821ff5ca0130a94
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2021-05-02 22:10:57 +0530

    [Unit] Add method to convert between units based on multipliers from file
    This way, users can add units that they want.

---
+++ @Unit/convert.m
@@ -0,0 +1,141 @@
+function valout = convertUnit(argin, fromUnit, toUnit)
+%convertUnit    Value in new units, based on info from txt file
+%   convertUnit(argin, fromUnit, toUnit) returns the value of argin in
+%   toUnit, assuming it was already in fromUnit. The conversion factors are
+%   read from a txt file. If a direct conversion isn't available, it will
+%   attempt to link several pairs of units to get the result.
+%   
+%   The order of the pairs in the file determines the path that will be
+%   taken while linking, so be careful while editing the file. This
+%   dependency will be removed in a future release.
+
+parentFolder = fileparts(mfilename('fullpath'));
+fileID = fopen(fullfile(parentFolder,'data','unitConversionFactors.txt'));
+
+scannedCells = textscan(fileID, '%s %s %f', 'CommentStyle','%');
+
+fclose(fileID);
+
+unitsMatrix = [scannedCells{1},scannedCells{2}];
+
+multiplicationFactor = multiplier(fromUnit, toUnit, unitsMatrix, scannedCells{3});
+
+if isempty(multiplicationFactor)
+    warning('AD:UnitConversion:badUnits', ...
+        ['Conversion from ''%s'' to ''%s'' not available (yet). Please '...
+         'type ''edit data/unitConversionFactors.txt'' at the command '...
+         'prompt to see list of available units.\nReturning the value '...
+         'unchanged'],...
+         fromUnit, toUnit)
+     valout = argin;
+     return
+end
+
+valout = multiplicationFactor * argin;
+
+end
+
+function val = multiplier(fromUnit, toUnit, unitsMatrix, multiplierArray)
+%multiplier     Mulitiplication factor computed recursively as chain.
+%   multiplier(fromUnit, toUnit, unitsMatrix, multiplierArray) returns the
+%   number to be multiplied to convert a quantity in fromUnit to toUnit,
+%   based on the unit pairing information in unitsMatrix, and the
+%   corresponding list of conversion factors in multiplierArray.
+
+% set default output as empty (NOT 1 because units may be incompatible)
+val = [];
+
+% if the From and To units are same (due to recursion or user input),
+% return 1 and terminate
+if strcmpi(fromUnit, toUnit), val = 1; return, end
+
+% locate From and To units in matrix
+[fromRows, fromCols] = find(ismember(unitsMatrix, fromUnit));
+[toRows, toCols] = find(ismember(unitsMatrix, toUnit));
+
+% return empty output if unable to locate
+if isempty(fromRows) || isempty(toRows), val = []; return, end
+
+% loop over each combination of From and To units in the matrix
+
+% !! -- MAKE NESTED LOOP FASTER BY COMPARING FROM AND TO ROWS LENGTH --- !!
+% !!! ---- SAVE ALL VAL TO REMOVE DEPENDENCY ON ORDER IN TXT FILE ---- !!!!
+
+% start outer loop
+for iFrom = 1:numel(fromRows)
+    
+    thisFromRow = fromRows(iFrom);
+    thisFromCol = fromCols(iFrom);
+    
+    % compute multiplier for this row (will be paired with corresponding To
+    % unit in the inner loop) which could be a reciprocal
+    multiplierFrom = ...
+        switchMultiplier(multiplierArray(thisFromRow), thisFromCol, true);
+    
+    % find name of other unit in this row (will be used as the next From
+    % unit in recursive call)
+    newFromUnit = unitsMatrix(thisFromRow, 3 - thisFromCol); 
+    % 3 - (1, 2) = (2, 1)
+    
+    % start inner loop
+    for iTo = 1:numel(toRows)
+        
+        % check if To unit is on the same row as From unit, and if so,
+        % return the multiplier computed earlier (potential reciprocal was
+        % taken care of then)
+        thisToRow = toRows(iTo);
+        if thisFromRow == thisToRow
+            val = multiplierFrom;
+            return
+        end
+        
+        % To unit is on a different row, so compute that row's multiplier
+        % like earlier (take care of reciprocals too)
+        thisToCol = toCols(iTo);
+        multiplierTo = ...
+            switchMultiplier(multiplierArray(thisToRow), thisToCol, false);
+        
+        % find name of other unit in this row as earlier
+        newToUnit = unitsMatrix(thisToRow, 3 - thisToCol);
+        
+        % create new matrix and list of multipliers by eliminating this
+        % From row and To row using setdiff
+        newUnitsMatrix = unitsMatrix(setdiff(1:end, [thisFromRow,thisToRow]),:);
+        newMultiplierArray = ...
+            multiplierArray(setdiff(1:end, [thisFromRow,thisToRow]));
+        
+        % recurse into this function to calculate the multiplier for
+        % converting between new units
+        fooVal = ...
+            multiplier(newFromUnit, newToUnit, newUnitsMatrix, newMultiplierArray);
+        
+        % if fooVal is empty, it means that the units are either
+        % incompatible, or the combination was bad and there's another one
+        % that yields a result
+        if ~isempty(fooVal) % we found the good combination, so return
+            val = multiplierFrom * fooVal * multiplierTo;
+            return % or else it will get overwritten in the next loop
+        end
+        
+    end % loop over To rows
+
+end % loop over From rows
+
+end % main function
+
+function valout = switchMultiplier(argin, col, isFrom)
+%switchMultiplier   Switches to 1/multiplier based on unit column location.
+%   switchMultiplier(argin, col, isFrom) returns either argin or 1/argin,
+%   depending on whether col is 1 or 2, and if isFrom is true or false.
+
+switch col
+    case 1
+        if isFrom, valout = argin; else, valout = 1/argin; end
+    case 2
+        if isFrom, valout = 1/argin; else, valout = argin; end
+%     otherwise
+%         disp(col)
+%         error('impossible column number')
+end
+
+end
