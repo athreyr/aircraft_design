f8b65d69a555468d80100eb5f7d36f1c233ad723
Athrey Ranjith Krishnanunni <72619383+athreyr@users.noreply.github.com>
2021-07-04 16:17:39 +0530

    [AtmosphericModel] Add unit conversion to geopotentialAltitude, edit doc

--- @AtmosphericModel/geopotentialAltitude.m
+++ @AtmosphericModel/geopotentialAltitude.m
@@ -1,19 +1,29 @@
-function h = altitudeGeopotential(ThisAtmosphericModel, altitudeGeometric, varargin)
-%altitudeGeopotential returns that from geometric altitude (in saved units)
-% 
-%   This method converts the input N-D array of geometric altitudes into
-%   their corresponding geopotential ones (of the same shape).
-% 
-%   Input units can optionally be specified, which allows the conversion of
-%   the input values into the units associated with the class object.
+function hVal = ...
+    geopotentialAltitude(ThisAtmosphericModel, altitudeGeometric, varargin)
+%geopotentialAltitude   Geopotential altitude from geometric, with units
+%   AM.geopotentialAltitude(altitudeGeometric, |unitIn, |unitOut) converts
+%   the N-D array of input geometric altitudes to geopotential ones (of the
+%   same shape). Both input and output units can optionally be specified.
+%   By default, they are both AM.SaveUnits.altitude. If you specify input
+%   unit and no output unit, output unit is same as input unit.
 
-hG = altitudeGeometric;
-if ~isempty(varargin)
-    hG = ThisAtmosphericModel.convertUnit(altitudeGeometric, varargin{1});
-end
+narginchk(2,4)
+
+SaveUnits = ThisAtmosphericModel.SaveUnits;
+% DispUnits = ThisAtmosphericModel.DisplayUnits;
+
+unitIn = setOptionalInputs({SaveUnits.altitude}, varargin);
+% only varargin(1), if it isn't empty, will be considered
+unitOut = setOptionalInputs({unitIn}, varargin(2:end));
+% unitOut = unitIn, unless user has specified something
+% [unitIn, unitOut] = ...
+%     setOptionalInputs({SaveUnits.altitude, DispUnits.altitude}, varargin);
+
+hG = Unit.convert(altitudeGeometric, unitIn, SaveUnits.radius);
 
 h =  hG * ThisAtmosphericModel.RADIUS_OF_EARTH ./ ...
     (hG + ThisAtmosphericModel.RADIUS_OF_EARTH);
 
-end
+hVal = Unit.convert(h, SaveUnits.radius, unitOut);
 
+end
